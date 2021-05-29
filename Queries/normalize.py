05d38328ac1af37cd0f572b0011984b99fdf261c
Kai-Chin Huang <qaz6209031@gmail.com>
2021-05-29 00:41:28 -0700

    Normalize all the professor related variables

--- Queries/normalize.py
+++ Queries/normalize.py
@@ -3,19 +3,19 @@ import re
 INPUT_FILENAME = 'normalized_1.txt'
 OUTPUT_FILENAME = 'normalized_final.txt'
 
-V_COURSE = 'course'
-V_PROFESSOR = 'professor'
+# V_COURSE = 'course'
+# V_PROFESSOR = 'professor'
 
 def main():
    var_pattern = r'\[([^\]]+)\]'
    var_set = set()
 
    # idea: use dictionary to normalize variable names?
-   var_dict = {
-      'course': V_COURSE,
-      'teacher': V_PROFESSOR,
-      'professor': V_PROFESSOR
-   }
+   # var_dict = {
+   #    'course': V_COURSE,
+   #    'teacher': V_PROFESSOR,
+   #    'professor': V_PROFESSOR
+   # }
 
    in_file = open(INPUT_FILENAME, 'r')
    out_file = open(OUTPUT_FILENAME, 'w')
@@ -23,6 +23,7 @@ def main():
    for line in in_file:
       for match in re.findall(var_pattern, line):
          match = match.lower()
+         match = normalize_var(match)
          var_set.add(match)
 
       line = line.split('|')
@@ -36,6 +37,15 @@ def main():
    print(var_set)
    in_file.close()
    out_file.close()
+
+def normalize_var(var):
+   profssor_key_words = ['professor', 'teacher', 'prof name', 'faculty', 'teachername', 'prof', 'csse-faculty', 'faculty']
+   # To do, come out with more keywords here
+   course_key_words = []
+   office_hour_key_words= []
+   if var in profssor_key_words:
+      var = 'professor'
+   return var
 
 if __name__ == '__main__':
    main()
