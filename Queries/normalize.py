c86bc5d3f13c3740fc84e74c54187087742f55cb
Ryan Ozawa <ryan.ozawa21@gmail.com>
2021-06-02 18:24:09 -0700

    add some functionality to normalizing inputs

--- Queries/normalize.py
+++ Queries/normalize.py
@@ -22,29 +22,38 @@ def main():
 
    for line in in_file:
       for match in re.findall(var_pattern, line):
-         match = match.lower()
-         match = normalize_var(match)
-         var_set.add(match)
-
+         repl = normalize_var(match)
+         var_set.add(repl)
+         line = line.replace(match, repl, 1)
+         
       line = line.split('|')
       question = line[1].strip()
       answer = line[2].strip()
 
-      out_file.write(question + '\n')
+      out_file.write(question + ' | ')
       out_file.write(answer + '\n')
          
    # test print to see how many variables there are
-   print(var_set)
+   #print(len(var_set))
    in_file.close()
    out_file.close()
+   
 
 def normalize_var(var):
-   profssor_key_words = ['professor', 'teacher', 'prof name', 'faculty', 'teachername', 'prof', 'csse-faculty', 'faculty']
+   profssor_key_words = ['professor', 'teacher', 'prof', 'name', 'faculty', 'teachername', 'prof', 'csse-faculty', 'faculty',
+                         'prof name', 'poly alias', 'alias', 'statistics-faculty', 'stats-faculty', 'user']
    # To do, come out with more keywords here
-   course_key_words = []
-   office_hour_key_words= []
+   course_key_words = ['coursecode', 'department code', 'roomname', 'coursenumorname', 'coursenumber', 'class', 'class-code',
+                       'major', 'csse-course', 'csse-courses', 'prereqs-csse-course', 'prereqs-stats-course', 'stats-course',
+                       'stats-section', 'csse-section', 'section']
+   bldg_key_words= ['room code', 'location', 'room', 'roomn', 'location', 'csse-faculty-office-location', 'stats-faculty-office-location']
+   var = var.lower()
    if var in profssor_key_words:
       var = 'professor'
+   elif var in course_key_words:
+      var = 'course'
+   elif var in bldg_key_words:
+      var = 'location'
    return var
 
 if __name__ == '__main__':
